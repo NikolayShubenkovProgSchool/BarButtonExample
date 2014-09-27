c45bd680fe447548f5282e2d713df1de80abc3eb
Nikolay Shubenkov <n.shubenkov@gmail.com>
2014-09-27 13:52:39 +0400

    Initial Commit

---
+++ BarButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BarButton
+//
+//  Created by n.shubenkov on 27/09/14.
+//  Copyright (c) 2014 n.shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
